ff87dffd9f1d33afe507d757d2305349b6f3adee
Michal Potoczny <michal.potoczny.92@gmail.com>
2020-07-15 20:27:42 +0200

    writetype npm added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Typewriter from 'typewriter-effect';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.changeDelay(100)
+              .typeString('W górze tyle gwiazd')
+              .pauseFor(500)
+              .changeDeleteSpeed(1)
+              .deleteAll().changeDeleteSpeed(1)
+              .typeString('W dole tyle miast')
+              .pauseFor(500)
+              .deleteAll()
+              .typeString('Gwiazdy miastom dają znać')
+              .deleteAll()
+              .typeString('Że dzieci muszą spaaać')
+              .pauseFor(1500)
+              .deleteAll()
+              .typeString('Ach śpiiij')
+              .deleteAll()
+              .typeString('Kochanie ! ')
+              .deleteAll()
+              .typeString('Jeśli gwiazdki z nieba chcesz')
+              .pauseFor(1)
+              .deleteAll()
+              .typeString('Dostaniesz <3')
+              .start();
+          }}
+        />
       </div>
     );
   }
