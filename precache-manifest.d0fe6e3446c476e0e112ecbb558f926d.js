add7081af1fc36cef64b49ec46773d798f49c45a
Michal Potoczny <michal.potoczny.92@gmail.com>
2020-07-15 21:22:12 +0200

    Updates

---
+++ precache-manifest.d0fe6e3446c476e0e112ecbb558f926d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cb3bea81b4f4bd66309",
+    "url": "/kolysanka/static/css/main.b2441fcf.chunk.css"
+  },
+  {
+    "revision": "6cb3bea81b4f4bd66309",
+    "url": "/kolysanka/static/js/main.8026bd49.chunk.js"
+  },
+  {
+    "revision": "39138f5986ee2bb7fc60",
+    "url": "/kolysanka/static/js/runtime~main.33920fd0.js"
+  },
+  {
+    "revision": "778bf3eb635c65743f3c",
+    "url": "/kolysanka/static/js/2.550cbff2.chunk.js"
+  },
+  {
+    "revision": "8942daf6ec6701805822127f99335ada",
+    "url": "/kolysanka/index.html"
+  }
+];
